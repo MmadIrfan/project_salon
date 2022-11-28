480ab12f15e5b9568e14b4c96b59a79e6b5b9142
irfan <muhammad.irfan.ti.20@cic.ac.id>
2022-11-28 17:37:52 +0700

    PP

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment(0, 2),
+            colors: <Color>[
+              Color(0xfff966be),
+              Color(0xfffc7fc7),
+              Color(0xffff96d0),
+              Color(0xffffacda),
+              Color(0xffffc1e3),
+              Color(0xffffd6ec),
+              Color(0xffffebf6),
+              Color(0xffffffff),
+            ],
+            tileMode: TileMode.mirror,
+          ),
+        ),
+        child: Row(children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(children: [
+                Image.asset(
+                  'assets/images/Logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+              ]),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
