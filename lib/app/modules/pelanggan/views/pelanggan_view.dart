a83ce278f447763137a9d4b3bcfc7669d5e02f07
alfan <muhamadalfan12maret@gmail.com>
2023-02-09 16:27:16 +0700

    beres

--- lib/app/modules/pelanggan/views/pelanggan_view.dart
+++ lib/app/modules/pelanggan/views/pelanggan_view.dart
@@ -17,8 +17,10 @@ class PelangganView extends GetView<PelangganController> {
           begin: Alignment.topCenter,
           end: Alignment(0, 2),
           colors: <Color>[
-            Color(0xfff966be),
+            Color.fromARGB(255, 250, 70, 178),
             Color(0xfffc7fc7),
+            Color(0xffffffff),
+            Color(0xffffffff),
             Color(0xffffffff),
             Color(0xffffffff),
             Color(0xffffffff),
@@ -33,18 +35,19 @@ class PelangganView extends GetView<PelangganController> {
         ),
       ),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           leading: Container(
               decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Colors.white),
+                border: Border.all(width: 1, color: Colors.black),
                 borderRadius: BorderRadius.circular(100),
                 color: Colors.white,
               ),
-              child: Image.asset("assets/images/logo.png")),
-          backgroundColor: Color(0xfff966be),
+              child: Image.asset("assets/images/Logo.png")),
+          backgroundColor: const Color(0xfff966be),
           title: const Text(
             'Wul@n Beauty Salon',
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
           ),
           elevation: 0.0,
           actions: <Widget>[
@@ -53,120 +56,162 @@ class PelangganView extends GetView<PelangganController> {
               padding: const EdgeInsets.all(3.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.all(Radius.circular(
+                borderRadius: const BorderRadius.all(Radius.circular(
                         8.0) //                 <--- border radius here
                     ),
               ),
-              child: Text('Karyawan'),
+              child: const Text('Karyawan'),
             )
           ],
         ),
         backgroundColor: Colors.transparent,
-        body: Column(children: <Widget>[
-          Container(
-            height: 100,
-            margin: EdgeInsets.only(top: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(
-                  20.0,
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 ),
               ),
-            ),
-            padding: const EdgeInsets.all(25.0),
-            child: TextField(
+              padding: const EdgeInsets.all(15.2),
+              child: TextField(
                 decoration: InputDecoration(
-                    labelText: "Pencarian",
-                    hintText: "Cari Pelanggan",
-                    prefixIcon: Icon(Icons.search),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                        borderSide: BorderSide(color: Color(0xfff966be))))),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 20),
-            height: 414,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment(0, 2),
-                colors: <Color>[
-                  Color(0xfff966be),
-                  Color(0xfffc7fc7),
-                  Color(0xffff96d0),
-                  Color(0xffffd6ec),
-                  Color(0xffffebf6),
-                  Color(0xffffffff),
-                ],
-                tileMode: TileMode.mirror,
-              ),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(
-                  30.0,
+                  labelText: "Pencarian",
+                  hintText: "Cari barang disini...",
+                  prefixIcon: const Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: const BorderSide(color: Color(0xfff966be)),
+                  ),
                 ),
               ),
             ),
-            child: Column(children: <Widget>[
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        const Text(
-                          "Data Pelanggan",
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1.0),
-                        ),
-                      ],
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(top: 20),
+                height: 600,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment(0, 2),
+                    colors: <Color>[
+                      Color(0xfff966be),
+                      Color(0xfffc7fc7),
+                      Color(0xffff96d0),
+                      Color(0xffffd6ec),
+                      Color(0xffffebf6),
+                      Color(0xffffffff),
+                    ],
+                    tileMode: TileMode.mirror,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(
+                      30.0,
                     ),
                   ),
-                ],
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                decoration: const BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
-                  ),
                 ),
-                padding: const EdgeInsets.all(2.2),
-                height: 300,
-                child: ListView.builder(
-                    itemCount: 5,
-                    itemBuilder: (context, i) {
-                      return Card(
-                        shape: RoundedRectangleBorder(
-                          side: const BorderSide(
-                            color: Colors.black,
+                child: Column(
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                "Pelanggan",
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 1.0),
+                              ),
+                            ],
                           ),
-                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                        child: const ListTile(
-                          title: Text(
-                            "Pelanggan",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: const BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20.0),
+                              topRight: Radius.circular(20.0),
+                            ),
                           ),
-                          subtitle: Text('Rp.xxx.xxx.xxx'),
+                          padding: const EdgeInsets.all(2.2),
+                          height: 260,
+                          child: ListView.builder(
+                              itemCount: 10,
+                              itemBuilder: (context, i) {
+                                return Card(
+                                  shape: RoundedRectangleBorder(
+                                    side: const BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: const ListTile(
+                                    title: Text(
+                                      "Pelanggan",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    subtitle: Text('Biaya : Rp.xxx.xxx.xxx'),
+                                  ),
+                                );
+                                // ignore: dead_code
+                              }),
                         ),
-                      );
-                    }),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 20),
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                const Text(
+                                  "Total Biaya : Rp.xxx.xxx.xxx",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 1.0),
+                                ),
+                                const SizedBox(height: 30),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      fixedSize: const Size(70, 32),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(50))),
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Tambah',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 9,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-              Container(),
-            ]),
-          ),
-        ]),
+            ),
+          ],
+        ),
       ),
     );
   }
