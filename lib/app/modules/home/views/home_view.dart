43550a4f9a5469ac67725b5b3d2ef04cc8331432
alfan <muhamadalfan12maret@gmail.com>
2023-02-09 12:48:53 +0700

    tambah

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -178,7 +178,7 @@ class HomeView extends GetView<HomeController> {
                                   ),
                                 );
                               }),
-                        )
+                        ),
                       ],
                     )
                   ],
